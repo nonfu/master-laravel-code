375e382de17a9688fc50dfb0a3c47bc2ca11cd24
nonfu <lvbailvzhanzhe@gmail.com>
2020-05-23 22:43:16 +0800

    exception

--- function/test.php
+++ function/test.php
@@ -50,3 +50,25 @@ $n1 = pow(100, 2); // 10000
 $n2 = sqrt(100);   // 10
 $n3 = decbin(100);  // 转化为二进制
 $n4 = mt_rand(0, 100);  // 生成0-100之间的随机数
+
+// 文件系统函数
+file_put_contents('test1.txt', '你好，学院君');  // 快速写入内容到文件 test.txt（不存在则自动创建）
+$content = file_get_contents('test1.txt');  // 从文件 test.txt 中读取内容
+var_dump($content);
+
+$file = fopen('test2.txt', 'w');   // 以写入模式打开文件 test2.txt，不存在则创建自动创建
+fwrite($file, '你好，');   // 通过 fwrite 写入内容到文件
+fwrite($file, '学院君！');  // 继续写入
+fclose($file);  // 关闭这个文件句柄
+
+$file = fopen('test2.txt', 'r');  // 只读模式打开 test2.txt 文件
+$content = '';
+while (!feof($file)) {    // 还没有到文件末尾，则继续读取
+    $content .= fread($file, 1024);   // 通过 fread 读取指定字节内容
+}
+fclose($file);
+var_dump($content);
+
+// 删除上述文件
+unlink('test1.txt');
+unlink('test2.txt');
