13ee15a41cfefb3482a5931d8ca0520375f81f85
nonfu <lvbailvzhanzhe@gmail.com>
2020-05-27 16:34:11 +0800

    mysqli

--- mysql/mysqli.php
+++ mysql/mysqli.php
@@ -11,10 +11,16 @@ $conn = mysqli_connect($host, $user, $password, $dbname, $port);
 mysqli_set_charset($conn, 'utf8mb4');
 
 // 在连接实例上进行查询
-$sql = 'SELECT * FROM `post`';
+$sql = 'SELECT * FROM `post` WHERE id = 1';
 $res = mysqli_query($conn, $sql);
 
 // 获取所有结果
-$rows = mysqli_fetch_all($res);
+/*
+$rows = mysqli_fetch_all($res, MYSQLI_ASSOC);
 echo '<pre>';
-var_dump($rows[2]);
+var_dump($rows);*/
+
+// 获取单条结果
+$row = mysqli_fetch_row($res);
+echo '<pre>';
+var_dump($row);
